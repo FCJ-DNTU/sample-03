10926a7b80493cb1d0d546bde39da89519127ae4
FromSunNews <phuongtu590@gmail.com>
2024-08-08 09:08:22 +0700

    config(project): Initial project

---
+++ frontend/src/utils/api/variables.js
@@ -0,0 +1,8 @@
+export const variables = {
+    BASE_URL: "http://your_fe_ipv4:5214/api/",
+    USER_API: "http://your_fe_ipv4:5214/api/user",
+    PRODUCT_API: "http://your_fe_ipv4:5214/api/product",
+    ORDER_API: "http://your_fe_ipv4:5214/api/order",
+    PRODUCTSIZE_API: "http://your_fe_ipv4:5214/api/productsize",
+    ORDERITEM_API: "http://your_fe_ipv4:5214/api/orderitem",
+}
